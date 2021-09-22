4e9e26010155403f8bf3ff9a1285eda4ec43e3c6
jenhuynh <jendoubleh@gmail.com>
2021-09-22 15:14:23 -0700

    Add forecast route

--- server/server.mjs
+++ server/server.mjs
@@ -3,9 +3,44 @@ import mime from "mime-types";
 
 import taskRouter from "./taskRouter.mjs";
 
+import forecastRouter from "./forecastRouter.mjs";
+
 const app = express();
 
-app.use("/api/tasks", taskRouter);
+// const fetch = require("node-fetch");
+// app.use("/api/tasks", taskRouter);
+
+app.use("/api/forecast", forecastRouter);
+
+// app.get("/api/forecast", (request, response) => {
+//   fetch(
+//     "https://api.openweathermap.org/data/2.5/forecast?q=Seattle&appid=2a0d2423a6b833f34886eef2eb9b0175",
+//   ).then((response) => {
+//     // const test = await res.json();
+//     // console.log(test);
+//     // response.json(test);
+//     // console.log(res);
+//     // response.json(res);
+//     response.json();
+//     // .then(res) => response.json)
+//     // .then(a => {consolelog(a), // do stuff here}
+//   });
+// });
+
+// app.get("/api/forecast", async (request, response) => {
+//   await fetch(
+//     "https://api.openweathermap.org/data/2.5/forecast?q=Seattle&appid=2a0d2423a6b833f34886eef2eb9b0175",
+//   ).then((response) => {
+//     // const test = await res.json();
+//     // console.log(test);
+//     // response.json(test);
+//     // console.log(res);
+//     // response.json(res);
+//     response.json();
+//     // .then(res) => response.json)
+//     // .then(a => {consolelog(a), // do stuff here}
+//   });
+// });
 
 // Do not comment out or delete this end point. The React development server
 // won't start until it pings this end point successfully.
